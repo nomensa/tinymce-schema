0d2b03d473976faaf5a7f1d185c765f7df12735f
Andrew Nevins <anevins@nomensa.com>
2018-04-03 17:07:56 +0100

    ICEW-44: Added ability to add microdata/ schema to highlighted content

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import plugin from './plugin';
 
-tinymce.PluginManager.add('microdata', plugin);
+tinymce.PluginManager.add('microdata', function (editor, url) {
+
+  editor.addButton('microdata', {
+    title: 'Add microdata',
+    text: 'Add microdata',
+    onclick: function () {
+      var bodyOptions = {
+        type: 'textbox',
+        name: 'microdata',
+        label: 'Schema property:'
+      };
+
+      // Opens a HTML page inside a TinyMCE dialog
+      editor.windowManager.open({
+        title: 'Adding schema',
+        body: bodyOptions,
+        onsubmit: function(event) {
+          /*
+           * Wraps selected content in a span that contains the microdata
+           * Constraints: Only works for itemprop microdata.
+           */
+          var selectedContent = tinymce.activeEditor.selection.getContent(),
+            microdataValue = event.data.microdata,
+            newMarkup = '<span itemprop="' + microdataValue + '">' + selectedContent + '</span>';
+
+          tinymce.activeEditor.selection.setContent(newMarkup);
+        }
+      });
+    }
+  });
+});
